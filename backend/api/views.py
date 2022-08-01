b05245787ec1892f30e2cf277d78d4473ea8262b
Akram <hfaiedhakram@gmail.com>
2022-08-01 21:27:38 +0100

    django view to drf view with serilizers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,24 +1,20 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
-# Create your views here.
+from products.serializers import ProductSerializer
 
+@api_view(["GET","POST"])
 def api_home(request,*args, **kwargs):
-
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    Django Rest Framework API View
+    """
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        # data = model_to_dict(model_data)
-        data = model_to_dict(model_data,fields=['id','title','price'])
-        # print(data)
-        # json_data_str = json.dumps(data)
-        # data['id']= model_data.id
-        # data['title']= model_data.title
-        # data['content']= model_data.content
-        # data['price']= model_data.price
+    if instance:
+        data = ProductSerializer(instance).data
+        # data = model_to_dict(model_data,fields=['id','title','price'])
         #* serialization: model instance(model_data) -> Python dic -> return Json to client
-    return JsonResponse(data)
-    # return HttpResponse(json_data_str,headers={"content-type":"application/json"})
+    return Response(data)
 
